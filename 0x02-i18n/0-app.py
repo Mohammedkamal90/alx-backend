a83fbed60a8fd6f3d5a7f0841638fb97a2c9ce1b
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-04-09 19:35:10 +0000

    task0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
-"""Simple flask application"""
+"""
+module define basic Flask app with single route
+"""
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+
+def index() -> str:
+    """
+    Render index.html template
+
+    Returns:
+        str: Rendered HTML content.
+    """
     return render_template('0-index.html')
+
+
+@app.route('/')
+def main() -> str:
+    """
+    Route for the main page.
+
+    Returns:
+        str: Rendered HTML content.
+    """
+    return index()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
